5f650c4fac955e34f47da12831b65a7abb09f01a
JubrilKolade <jubrilkolade97@gmail.com>
2024-11-11 03:17:21 +0000

    guessing game

--- src/main.rs
+++ src/main.rs
@@ -7,54 +7,65 @@ fn main() {
     let correct = rand::thread_rng().gen_range(1..=10);
     println!("correct: {correct}");
     println!("Hi, make a guess?");
-    let mut guess = String::new();
+    
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("error reading input");
+    loop { 
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("error reading input");
 
-    let guess: u32 = guess.trim().parse().expect("error with parse");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(e) => {
+                println!("Error with parse, try again. {e}"); 
+                continue;
+            }
+        };
 
 
-   //CONTROL FLOW 
-   //this
-    // if correct < guess {
-    //     println!("you guessed higher")
-    // } else if correct > guess {
-    //     println!("you guessed lower")
-    // } else {
-    //     println!("you guessed right")
-    // };
+    //CONTROL FLOW 
+    // THIS
+        // if correct < guess {
+        //     println!("you guessed higher")
+        // } else if correct > guess {
+        //     println!("you guessed lower")
+        // } else {
+        //     println!("you guessed right")
+        // };
 
-    //or this
-    // let mut message = String::new();
-    // if correct < guess {
-    //     message = String::from("you guessed too high");
-    // } else if correct > guess {
-    //     message = String::from("you guessed too low");
-    // } else {
-    //     message = String::from("you guessed just right");
-    // }
+        //OR THIS
+        // let mut message = String::new();
+        // if correct < guess {
+        //     message = String::from("you guessed too high");
+        // } else if correct > guess {
+        //     message = String::from("you guessed too low");
+        // } else {
+        //     message = String::from("you guessed just right");
+        // }
 
-    // println!("{message}");
+        // println!("{message}");
 
-    //or this
-    // let mut message = if correct < guess {
-    //     String::from("you guessed too high")
-    // } else if correct > guess {
-    //      String::from("you guessed too low")
-    // } else {
-    //      String::from("you guessed just right")
-    // };
+        //OR THIS
+        // let mut message = if correct < guess {
+        //     String::from("you guessed too high")
+        // } else if correct > guess {
+        //      String::from("you guessed too low")
+        // } else {
+        //      String::from("you guessed just right")
+        // };
 
-    // println!("{message}");
+        // println!("{message}");
 
-    //or this
-    let message = match  guess.cmp(&correct) {
-        Ordering::Greater => "you guessed too high",
-        Ordering::Less => "you guessed too low",
-        Ordering::Equal => "you guessed just right",
-    };
+        //OR THIS
 
-    println!("{message}");
+        match  guess.cmp(&correct) {
+            Ordering::Greater => println!("you guessed too high"),
+            Ordering::Less => println!("you guessed too low"),
+            Ordering::Equal => {
+                println!("you guessed just right");
+                break;
+            } 
+        };
+    }
 }
